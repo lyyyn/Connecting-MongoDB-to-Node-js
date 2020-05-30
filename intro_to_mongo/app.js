cc86a56d6d80b24fcfb6ed9d5a11f88789894ba8
Linawaty <linawatylim@gmail.com>
2020-05-30 11:49:28 +0800

    find documents from collections

--- intro_to_mongo/app.js
+++ intro_to_mongo/app.js
@@ -10,55 +10,6 @@ const dbName = 'tweets';
 // Create a new MongoClient
 const client = new MongoClient(url, { useUnifiedTopology: true }); //this is to hide the warning
 
-//Many tweet
-const manyTweets = [{
-    title: 'Deep Thoughts',
-    body: 'Friends, I have been navel-gazing',
-    author: 'Karolin'
-},
-{
-    title: 'Sage Advice',
-    body: 'Friends, I am vegan and so should you',
-    author: 'Karolin',
-    likes: 20
-},
-{
-    title: 'Whole Reality',
-    body: 'I shall deny friendship to anyone who does not exclusively shop at Whole Foods',
-    author: 'Karolin',
-    likes: 40
-},
-{
-    title: 'Organic',
-    body: 'Friends, I have spent $2300 to be one of the first people to own an organic smartphone',
-    author: 'Karolin',
-    likes: 162
-},
-{
-    title: 'Confusion',
-    body: 'Friends, why do you just respond with the word `dislike` ? Surely you mean to click the like button?',
-    author: 'Karolin',
-    likes: -100
-},
-{
-    title: 'Vespa',
-    body: 'Friends, my Vespa has been upgraded to run on old french fry oil. Its top speed is now 11 mph',
-    author: 'Karolin',
-    likes: 2
-},
-{
-    title: 'Licensed',
-    body: 'Friends, I am now officially licensed to teach yogalates. Like this to get 10% off a private lesson',
-    author: 'Karolin',
-    likes: 3
-},
-{
-    title: 'Water',
-    body: 'Friends, I have been collecting rain water so I can indulge in locally sourced raw water. Ask me how',
-    author: 'Karolin',
-},
-];
-
 // Use connect method to connect to the Server
 client.connect((err) => {
     assert.equal(null, err);
@@ -66,13 +17,19 @@ client.connect((err) => {
 
     let db = client.db(dbName);
     const collection = db.collection('tweets');
-    collection.insertMany(manyTweets, (err, result) => {
-        assert.equal(null, err);
-        assert.equal(manyTweets.length, result.insertedCount);
-        console.log(`successfully insert ${manyTweets.length} doc(s) into collection`);
-        client.close(); //need to close db at the collection - async
-    });
 
+    //find all and store in Array
+    collection.find({}, {
+        projection: { 
+            title: 1,
+            author:1,
+            _id: 0 }
+    }).toArray((err, docs) => {
+        assert.equal(err, null);
+        console.log(`Found the ${docs.length} documents:`);
+        console.log(docs);
+        client.close();
+    });
 
 });
 
