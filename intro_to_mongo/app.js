4ce8e2dfe0443580afe48021b85ba8f057ee27f4
Linawaty <linawatylim@gmail.com>
2020-05-30 11:40:12 +0800

    insert many docs into collection

--- intro_to_mongo/app.js
+++ intro_to_mongo/app.js
@@ -10,12 +10,54 @@ const dbName = 'tweets';
 // Create a new MongoClient
 const client = new MongoClient(url, { useUnifiedTopology: true }); //this is to hide the warning
 
-//My first tweet
-const myFirstTweet = {
+//Many tweet
+const manyTweets = [{
     title: 'Deep Thoughts',
     body: 'Friends, I have been navel-gazing',
     author: 'Karolin'
-}
+},
+{
+    title: 'Sage Advice',
+    body: 'Friends, I am vegan and so should you',
+    author: 'Karolin',
+    likes: 20
+},
+{
+    title: 'Whole Reality',
+    body: 'I shall deny friendship to anyone who does not exclusively shop at Whole Foods',
+    author: 'Karolin',
+    likes: 40
+},
+{
+    title: 'Organic',
+    body: 'Friends, I have spent $2300 to be one of the first people to own an organic smartphone',
+    author: 'Karolin',
+    likes: 162
+},
+{
+    title: 'Confusion',
+    body: 'Friends, why do you just respond with the word `dislike` ? Surely you mean to click the like button?',
+    author: 'Karolin',
+    likes: -100
+},
+{
+    title: 'Vespa',
+    body: 'Friends, my Vespa has been upgraded to run on old french fry oil. Its top speed is now 11 mph',
+    author: 'Karolin',
+    likes: 2
+},
+{
+    title: 'Licensed',
+    body: 'Friends, I am now officially licensed to teach yogalates. Like this to get 10% off a private lesson',
+    author: 'Karolin',
+    likes: 3
+},
+{
+    title: 'Water',
+    body: 'Friends, I have been collecting rain water so I can indulge in locally sourced raw water. Ask me how',
+    author: 'Karolin',
+},
+];
 
 // Use connect method to connect to the Server
 client.connect((err) => {
@@ -24,10 +66,10 @@ client.connect((err) => {
 
     let db = client.db(dbName);
     const collection = db.collection('tweets');
-    collection.insertOne(myFirstTweet, (err, result) => {
+    collection.insertMany(manyTweets, (err, result) => {
         assert.equal(null, err);
-        assert.equal(1, result.insertedCount);
-        console.log('successfully insert a doc into collection');
+        assert.equal(manyTweets.length, result.insertedCount);
+        console.log(`successfully insert ${manyTweets.length} doc(s) into collection`);
         client.close(); //need to close db at the collection - async
     });
 
